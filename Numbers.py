f6cd3ba8046efab91953f60e8a2ea3fde782ca44
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-23 18:34:59 +0600

    Boolean Numbers and Casting are added

---
+++ Numbers.py
@@ -0,0 +1,11 @@
+import random
+
+x=1
+y=2.8
+z=1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+#printing random
+print(random.randrange(1,117))
---
+++ casting.py
@@ -0,0 +1,8 @@
+x=int(1.5)
+y=int(2.8)
+z=int("3")
+
+
+print(type(x))
+print(type(y))
+print(type(z))
