a889856299f1375e1cd8788bbf4f00d172146b3f
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 17:33:43 +0600

    ARRAY IS ADDED

---
+++ Array.py
@@ -0,0 +1,19 @@
+import array as arr
+
+
+cars=["BMW","TOYOTA","HYUNDAI"]
+
+x=cars[0]
+
+print(cars[0])
+
+y=len(cars)
+
+print(y)
+
+cars.append("FORD")
+
+cars.pop(0)
+cars.remove("TOYOTA")
+
+print(len(cars))
