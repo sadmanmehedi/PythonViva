282fc7ddb131c013ed3b7652722dcbf876a1e2d3
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 14:24:39 +0600

    Added if

---
+++ If.py
@@ -0,0 +1,39 @@
+a=17
+b=10
+
+if a>b:
+    print("Sadman")
+elif a==b:
+    print("Sivan")
+else:
+    print("Mehedi")
+
+
+#ShortHandIF
+x=2
+y=3
+print("A") if x>y else print("B")
+
+p=22
+q=33
+print("AFS") if a>b else print("=") if a==b else print("B")
+
+
+
+g=4231
+h=23455
+if not g<h:
+    print("GEAS")
+
+
+#Nested IF
+
+x=40
+
+
+if x>10:
+   print("Above ten,")
+   if x>20:
+       print("And Also above 20")
+   else:
+       print("but not above 20")
