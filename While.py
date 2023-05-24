df2ecf662335621b72e0fa11df7213db11b2957c
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 14:31:00 +0600

    While added

---
+++ While.py
@@ -0,0 +1,62 @@
+i=1
+
+while i<6:
+    print(i)
+    i+=1
+
+
+#Break
+i=1
+while i<6:
+    print(i)
+    if i==3:
+        break
+    i+=1
+
+#Continue
+i = 1
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
