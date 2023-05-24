7881d42dcbf346c0d4be7ab74941557c0878ead8
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 12:35:44 +0600

    Set is done

---
+++ Sets.py
@@ -0,0 +1,26 @@
+FirstSet={"VivaSoft","Optimizily","SAMSUNG"}
+print(FirstSet)
+
+#It can also take multiple values,can show us its type also can be initialized by constructor
+#We need loop for printing the elements of the set
+
+#For Adding
+FirstSet.add("Intelligent Machines")
+
+print(FirstSet)
+
+#Also possible to add a list with Set
+
+#Removing Set Items
+
+thisset={"apple","mango"}
+thisset.remove("mango")
+print(thisset)
+
+set1={"a","bv","fa","gfeqw"}
+set2={253,235,232}
+set3=set1.union(set2)
+print(set3)
+
+
+
