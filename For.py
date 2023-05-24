7b7a769dc1061023acc7b6e1935e19e76c8f3cdc
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 14:40:37 +0600

    For Loop DOne

---
+++ For.py
@@ -0,0 +1,23 @@
+cars=["BMW","AUDI","TOYOTA","HYUNDAI","MERCEDEZ BENZ"]
+
+#IN A LIST
+for x in cars:
+    print(x)
+
+#IN A STRING
+for x in "banana":
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+for x in range(6):
+    print("I Needed You")
+
+
+
+for x in range(2, 6):
+  print(x)
