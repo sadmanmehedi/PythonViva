d3dba74b6bb59c928f465fe79c3805b49bec5da7
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 11:27:05 +0600

    List updated

--- List.py
+++ List.py
@@ -32,3 +32,29 @@ print(type(Multiple))
 sivan=list((22,33,44))
 
 print(sivan)
+
+#Accesing Items
+normal=["VivaSoft","Optimizely","RedDot","Samsung"]
+print(normal[0])
+print(normal[-1])
+print(normal[-2])
+print(normal[0:2])
+print(normal[:1])
+print(normal[0:])
+
+
+#Modifying Value
+modification=["Chelsea","Arsenal","Spurs","Fulham"]
+print(modification)
+modification[3]="Brentford"
+print(modification)
+
+#We Can Modify a range of values too
+modification[1:3]=["Crystal Palace","Nottingham Forrest"]
+print(modification)
+modification[1:2]=["Manchester City","Manchester United"]
+print(modification)
+
+#Insert Value
+modification.insert(0,"Liverpool")
+print(modification)
