3d9085f3cf93a0c81228166491e5c0af7a9a1ce3
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-24 11:12:07 +0600

    Python list is added

---
+++ List.py
@@ -0,0 +1,34 @@
+#How to declare a list
+interns=["Sivan","Poushi","Samin","Talimul"]
+
+print(interns)
+
+#List can allow Duplicate Values too
+internsduplicate=["Sivan","Sivan","Poushi","Samin","Talimul"]
+
+print(internsduplicate)
+
+#With List I can find the length too
+print(len(interns))
+print(len(internsduplicate))
+
+#I can take multiple types of data here
+list1=["sivan","shammo","mouli"]
+list2=[22,324,523,325,523]
+list3=[True,False,False,True]
+
+print(list1)
+print(list2)
+print(list3)
+
+# A list can have multiple types of data types too
+Multiple=["Sivan",1999,True]
+print(Multiple)
+
+#The Type
+print(type(Multiple))
+
+#Assigning with Constructor
+sivan=list((22,33,44))
+
+print(sivan)
