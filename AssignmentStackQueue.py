40f27525d0f3bb83dc0a3dfd3540783b44dd21fb
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-26 16:59:54 +0600

    Assignment is complete

--- AssignmentStackQueue.py
+++ AssignmentStackQueue.py
@@ -1,8 +1,10 @@
+import sys
+
+
 print("Hello There! What do you want to Implement?")
 print("1.Queue 2.Stack")
 print("\n")
 choice=int(input())
-print(choice)
 
 l=[]
 
@@ -12,21 +14,38 @@ while True :
     print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print\n6.Clear\n7.Exit")
     subchoice=int(input());
     if subchoice==1:
-        print("Which Value you want to push?")
-        value=int(input())
-        l.append(value)
-        print("Operation Done!")
+        print("How Many Values to  Push?")
+        Howmany=int(input())
+        print("Okay Input the Values")
+        check=0
+        while check<Howmany:
+           value=int(input())
+           l.append(value)
+           check=check+1
+
+        print("Got the value")
         print("Updated Queue is:")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask=int(input())
+        if ask==2:
+            print("Thank you!")
+            sys.exit()
 
-    elif subchoice==2:
+    elif subchoice == 2:
         if len(l)==0:
            print("Queue is already Empty")
+
         else:
             print("Value is popped from the first")
             print("Updated Queue is:")
             l.pop(0)
             print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice == 3:
         print("Tell me the index and the value you want to insert\n")
         print("Index?\n")
@@ -37,14 +56,34 @@ while True :
         print("Operation Done")
         print("Updated Queue is:")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice==4:
         print("The size of the Queue is "+ str(len(l)))
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice==5:
         print("The Queue is")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice==6:
         l.clear()
         print("Queue is empty now!")
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     else:
         break
 
@@ -52,20 +91,37 @@ while True :
     print("Which Stack operation you want to perform")
     print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print\n6.Clear\n7.Exit")
     subchoice=int(input());
-    if subchoice==1:
-        print("Which Value you want to push?")
-        value=int(input())
-        l.append(value)
+    if subchoice == 1:
+        print("How Many Values to  Push?")
+        Howmany = int(input())
+        print("Okay Input the Values")
+        check = 0
+        while check < Howmany:
+            value = int(input())
+            l.append(value)
+            check = check + 1
+
+        print("Got the value")
         print("Updated Stack is:")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice==2:
         if len(l)==0:
            print("Stack is already Empty")
         else:
             print("Value is popped from the last")
             print("Updated Stack is:")
-            l.pop(0)
+            l.pop()
             print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
 
     elif subchoice == 3:
         print("Tell me the index and the value you want to insert\n")
@@ -76,14 +132,34 @@ while True :
         l.insert(index, value)
         print("Updated Stack is:")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice == 4:
         print("The size of the Stack is " + str(len(l)))
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice == 5:
         print("The Stack is")
         print(l)
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     elif subchoice == 6:
         l.clear()
         print("Stack is empty now!")
+        print("Back to Main Menu?\n1.Yes\n2.No Terminate The Program")
+        ask = int(input())
+        if ask == 2:
+            print("Thank you!")
+            sys.exit()
     else:
         break
 
