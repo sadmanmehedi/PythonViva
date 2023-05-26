356cc0f3eafcd327b09dbba7caafc8ef0a8ff3dc
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-26 16:23:04 +0600

    Assignment Updated

--- AssignmentStackQueue.py
+++ AssignmentStackQueue.py
@@ -5,53 +5,90 @@ choice=int(input())
 print(choice)
 
 l=[]
-print(type(l))
 
-if choice==1:
-    print("So you want to perform Queue")
+while True :
+  if choice==1:
     print("Which Queue operation you want to perform")
-    print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print")
+    print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print\n6.Clear\n7.Exit")
     subchoice=int(input());
     if subchoice==1:
         print("Which Value you want to push?")
         value=int(input())
+        l.append(value)
+        print("Operation Done!")
+        print("Updated Queue is:")
+        print(l)
+
     elif subchoice==2:
-        print("Value is popping from the first")
+        if len(l)==0:
+           print("Queue is already Empty")
+        else:
+            print("Value is popped from the first")
+            print("Updated Queue is:")
+            l.pop(0)
+            print(l)
     elif subchoice == 3:
         print("Tell me the index and the value you want to insert\n")
         print("Index?\n")
         index=int(input())
         print("Value?\n")
         value = int(input())
+        l.insert(index, value)
+        print("Operation Done")
+        print("Updated Queue is:")
+        print(l)
     elif subchoice==4:
         print("The size of the Queue is "+ str(len(l)))
-    else:
+    elif subchoice==5:
         print("The Queue is")
         print(l)
+    elif subchoice==6:
+        l.clear()
+        print("Queue is empty now!")
+    else:
+        break
 
-else:
-    print("So you want to perform Stack")
+  else:
     print("Which Stack operation you want to perform")
-    print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print")
+    print("1.Push\n2.Pop\n3.Insert\n4.Size\n5.Print\n6.Clear\n7.Exit")
     subchoice=int(input());
     if subchoice==1:
         print("Which Value you want to push?")
         value=int(input())
+        l.append(value)
+        print("Updated Stack is:")
+        print(l)
     elif subchoice==2:
-        print("Value is popped from the last")
+        if len(l)==0:
+           print("Stack is already Empty")
+        else:
+            print("Value is popped from the last")
+            print("Updated Stack is:")
+            l.pop(0)
+            print(l)
+
     elif subchoice == 3:
         print("Tell me the index and the value you want to insert\n")
         print("Index?\n")
         index = int(input())
         print("Value?\n")
         value = int(input())
+        l.insert(index, value)
+        print("Updated Stack is:")
+        print(l)
     elif subchoice == 4:
         print("The size of the Stack is " + str(len(l)))
-    else:
+    elif subchoice == 5:
         print("The Stack is")
         print(l)
+    elif subchoice == 6:
+        l.clear()
+        print("Stack is empty now!")
+    else:
+        break
 
 
 
+print("Thank You!")
 
 
