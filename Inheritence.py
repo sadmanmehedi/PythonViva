e7d623ac9690d9b3ee02a2d3a03de4b4ddd27a99
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-29 14:19:17 +0600

    Inheritence Done

--- Inheritence.py
+++ Inheritence.py
@@ -6,7 +6,25 @@ class Person:
     def printname(self):
         print(self.firstname,self.lname)
 
+
 x=Person("Sadman","Mehedi")
 x.printname()
 
-class
+
+#By using Person init i will still be able to access the parent class's initiate function
+
+class Student(Person):
+    def __init__(self,fname,lname,year):
+        #Person.__init__(self,fname,lname)
+        #We can also do by using the Super Function
+        super().__init__(fname,lname)
+        self.graduationyear=year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lname, "to the class of", self.graduationyear)
+
+
+
+y=Student("Anam"," Raz",2023)
+print(y.welcome())
+
