4a74ab70b9ef72db716882d10628fc70b36e4f8a
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-05-31 14:23:45 +0600

    Inheritence Started

---
+++ inheritence2.py
@@ -0,0 +1,10 @@
+class PL:
+    def __init__(self,name,city,title):
+        self.name=name
+        self.city=city
+        self.title=title
+
+
+Chelsea=PL("Chelsea","London","4")
+Manchester=PL("ManchesterUnited","Manchester","14")
+Liverpool=PL("Liverpool","Mersyside","1")
